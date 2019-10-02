a799a6ff50dc735832a5d7a4c6ac7046deb36528
anusha <49363320+kshaanu@users.noreply.github.com>
2019-10-02 22:47:35 +0530

    Update samp.tf

--- samp.tf
+++ samp.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "${aws_access_key}"
-    secret_key = "${aws_secret_key}"
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
   }
   resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
